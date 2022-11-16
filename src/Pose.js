458d5d5bc3065b081ffa8eee0ce9574606f3f855
Tina Flower <tmflower@gmail.com>
2022-11-16 11:20:59 -1000

    add pose component

--- src/Pose.js
+++ src/Pose.js
@@ -1,1 +1,10 @@
-// TODO: write code to render a single pose that accepts props from Poses (this is a "dumb" component; no logic lives here)
+export function Pose(sanskritName, englishName, diagram, video) {
+    return (
+        <>
+            <h1>Yoga2Go</h1>
+            <h2>Welcome to your daily dose of yoga! Today's pose is <em>{sanskritName}</em></h2>
+            <h3>This commonly referred to as {englishName} pose in English.</h3>
+            <img src={diagram} style={{width: "400px"}}></img>
+        </>
+    )
+}
--- src/Poses.js
+++
@@ -1,1 +0,0 @@
-// TODO: write code that requests data from the yoga API and YouTube API and renders a single pose.
