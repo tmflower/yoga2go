eaeb993d3b6a7082a503fffba2bbed2a4ca6d62b
Tina Flower <tmflower@gmail.com>
2022-11-18 10:20:51 -1000

    update about and dev

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,17 @@
-// TODO: write code that renders a Dev component for each team member
+import { Dev } from "./Dev";
 
 export function About() {
     return (
         <>
-            <p>This page is about us!</p>
+            <h3>Meet the Yoga2Go Developers</h3>
+            <Dev name="Elizabeth King"/>
+            <Dev name="Heather Stout Svanidze"/>
+            <Dev name="Kate Baker"/>
+            <Dev name="Khalina Boyce"/>
+            <Dev name="Kris Jackson"/>
+            <Dev name="Sarah Allen"/>
+            <Dev name="Tina Flower"/>
+            <Dev name="Valentina Provida"/>
         </>
     )
 }
--- src/Components/Dev.js
+++ src/Components/Dev.js
@@ -1,1 +1,11 @@
-// TODO: write code that renders a single Dev component
+import { defaultForBio } from "../Components/Images/defaultForBio.jpg";
+
+export function Dev({ name, bio="All around awesome human!", photo={defaultForBio}}) {
+    return (
+        <div>
+            <img src={photo}></img>
+            <p>{name}</p>
+            <p>{bio}</p>            
+        </div>
+    )
+}
