3b5241f6f429d06ce64d1fab1a7d4b6536358a5c
ravenwooddesigns <ravenwooddesigns365@gmail.com>
2022-11-18 10:05:47 -0800

    Changes to Navbar.js, Home.js and App.css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,26 @@
 // TODO: Build Navbar component with NavLinks for each route
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import background from "./Images/Yoga2Go.png";
 
 export function Navbar() {
     return (
-        <>
-            <NavLink exact="true" to="/" end>Home</NavLink>
-            <NavLink exact="true" to="/explore" end>Explore</NavLink>
-            <NavLink exact="true" to="/about" end>About</NavLink>
-        </>
+        <nav className="navbar">
+            <div className="nav-container" div style={{ backgroundImage: `url(${background})`}}>
+            <NavLink exact="true" to="/" className="nav-logo">Yoga2Go</NavLink>
+            <ul className="nav-menu">
+                <li className="nav-item">
+                <NavLink exact="true" to="/" className="nav-links">Home</NavLink>                
+                </li>
+                <li className="nav-item">
+                <NavLink exact="true" to="/explore" className="nav-links">Explore</NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink exact="true" to="/about" className="nav-links">About</NavLink>
+                </li>
+            </ul>
+            
+            </div>            
+        </nav>
     )
 }
