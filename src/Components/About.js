58d6569d84e9925e241d62007ce09bed362f6ad7
Tina Flower <tmflower@gmail.com>
2022-11-18 15:06:26 -1000

    fine tuning

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,16 +3,18 @@ import "../styles/About.css";
 
 export function About() {
     return (
-        <div className="bio-container">
+        <div>
             <h1>Meet the Yoga2Go Developers</h1>
-            <Dev name="Elizabeth King"/>
-            <Dev name="Heather Stout"/>
-            <Dev name="Kate Baker"/>
-            <Dev name="Khalina Boyce"/>
-            <Dev name="Kris Jackson"/>
-            <Dev name="Sarah Allen"/>
-            <Dev name="Tina Flower"/>
-            <Dev name="Valentina Provida"/>
+            <div className="bio-container">
+                <Dev name="Elizabeth King"/>
+                <Dev name="Heather Stout"/>
+                <Dev name="Kate Baker"/>
+                <Dev name="Khalina Boyce"/>
+                <Dev name="Kris Jackson"/>
+                <Dev name="Sarah Allen"/>
+                <Dev name="Tina Flower"/>
+                <Dev name="Valentina Provida"/>
+            </div>
         </div>
     )
 }
