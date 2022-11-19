abdf3b7a9dc72c2b4923f39cdbe472fbd06c8bb7
elizabethkatk <elizabeth.kat.k@gmail.com>
2022-11-19 00:58:10 -0600

    Responsive styling on About

--- src/Components/Dev.js
+++ src/Components/Dev.js
@@ -1,26 +1,25 @@
 import defaultForBio from "../Components/Images/defaultForBio.jpg";
 import "../styles/Dev.css";
 
+export function Dev({
+  name,
+  bio,
+  photo = defaultForBio,
+  link = "https://unsplash.com/photos/klH-f7mw2Ws",
+}) {
+  return (
+    <div className="bio-profile">
+      <div>
+        <img src={photo} className="bio-image" alt={name}></img>
+        <div className="about-text">
+          <p className="bio-name">{name}</p>
+          <p className="bio-text">{bio}</p>
 
-export function Dev({ 
-    name, 
-    bio, 
-    photo=defaultForBio, 
-    link="https://unsplash.com/photos/klH-f7mw2Ws", }) {
-    return (
-        <div className="bio-profile">
-            
-            <div>
-                <img src={photo} width="200" alt={name}></img>  
-                
-                <p className="bio-name">{name}</p>
-                             
-                <p className="bio-text">{bio}</p>
-
-                <a href={link} target="_blank" rel="noreferrer">
-                    <button className="connect-btn">Connect with Me!</button>{" "}
-                </a>
-            </div>
+          <a href={link} target="_blank" rel="noreferrer" className="connect">
+            Connect with Me!{" "}
+          </a>
         </div>
+      </div>
+    </div>
   );
 }
