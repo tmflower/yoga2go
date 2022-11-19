fd7a13b518880e8eaa2b0f2fdebb61eaa5000182
elizabethkatk <elizabeth.kat.k@gmail.com>
2022-11-18 21:24:42 -0600

    Minor changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,19 +2,19 @@ import { Dev } from "./Dev";
 import "../styles/About.css";
 
 export function About() {
-    return (
-        <div>
-            <h1>Meet the Yoga2Go Developers</h1>
-            <div className="bio-container">
-                <Dev name="Elizabeth King"/>
-                <Dev name="Heather Stout"/>
-                <Dev name="Kate Baker"/>
-                <Dev name="Khalina Boyce"/>
-                <Dev name="Kris Jackson"/>
-                <Dev name="Sarah Allen"/>
-                <Dev name="Tina Flower"/>
-                <Dev name="Valentina Provida"/>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Meet the Yoga2Go Developers</h1>
+      <div className="bio-container">
+        <Dev name="Elizabeth King" />
+        <Dev name="Heather Stout" />
+        <Dev name="Kate Baker" />
+        <Dev name="Khalina Boyce" />
+        <Dev name="Kris Jackson" />
+        <Dev name="Sarah Allen" />
+        <Dev name="Tina Flower" />
+        <Dev name="Valentina Provida" />
+      </div>
+    </div>
+  );
 }
--- src/Components/Dev.js
+++ src/Components/Dev.js
@@ -1,23 +1,25 @@
 import defaultForBio from "../Components/Images/defaultForBio.jpg";
 import "../styles/Dev.css";
 
-export function Dev({ 
-    name, 
-    bio="All around awesome human!", 
-    photo=defaultForBio, 
-    link="https://unsplash.com/photos/klH-f7mw2Ws?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"}) {
-    return (
-        <div className="bio-profile">
-            
-            <div>
-                <img src={photo} width="200"></img>  
-                
-                <p className="bio-name">{name}</p>
-                             
-                <p className="bio-text">{bio}</p>
+export function Dev({
+  name,
+  bio = "All around awesome human!",
+  photo = defaultForBio,
+  link = "https://unsplash.com/photos/klH-f7mw2Ws?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+}) {
+  return (
+    <div className="bio-profile">
+      <div>
+        <img src={photo} width="200"></img>
 
-                <a href={link} target="_blank"><button className="connect-btn">Connect with Me!</button> </a> 
-            </div>                     
-        </div>
-    )
+        <p className="bio-name">{name}</p>
+
+        <p className="bio-text">{bio}</p>
+
+        <a href={link} target="_blank">
+          <button className="connect-btn">Connect with Me!</button>{" "}
+        </a>
+      </div>
+    </div>
+  );
 }
