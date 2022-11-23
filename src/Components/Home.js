0f555030aee3f2aa5a136b41a21db75e766ae398
Tina Flower <tmflower@gmail.com>
2022-11-23 11:01:51 -1000

    remove video api requests and hardcode videoIds

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,16 +6,13 @@ import { Pose } from "./Pose";
 import "../styles/Home.css";
 import background from "./Images/background.png";
 import { NavLink } from "react-router-dom";
+import videos from "./utils";
 
 export function Home() {
   const [pose, setPose] = useState(null);
-  // const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 47));
-  const randomNum = Math.floor(Math.random() * 47);
-  // const [matchingNum, setMatchingNum] = useState(randomNum);
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 47));
   const [poseName, setPoseName] = useState(null);
-  const [video, setVideo] = useState(null);
-
-  // const KEY=
+  const [video, setVideo] = useState("AB3Y-4a3ZrU");
 
   useEffect(() => {
     async function getPose() {
@@ -23,30 +20,15 @@ export function Home() {
         .get("https://lightning-yoga-api.herokuapp.com/yoga_poses")
         .then((res) => {
           setPose(res.data.items[randomNum]);
-          // setPoseName(res.data.items[matchingNum].sanskrit_name);
           setPoseName(res.data.items[randomNum].sanskrit_name);
         })
         .catch((error) => {
           console.error(error);
         });
-    }
+      }
     getPose();
-  }, []);
-
-  // console.log(poseName);
-
-//   useEffect(() => {
-//     async function getVideo() {
-//         const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=how+to+do+${poseName}&maxResults=1&key=${KEY}`)
-//         .then((res) => {
-//                 setVideo(res.data.items[0].id.videoId)             
-//         })
-//         .catch((error) => {
-//             console.error(error)
-//         })           
-//     }
-//     getVideo()
-// }, [poseName]);
+    setVideo(videos[poseName]);
+  }, [randomNum, pose]);
 
   return (
     <div style={{ backgroundImage: `url(${background})` }}>
@@ -62,6 +44,7 @@ export function Home() {
             sanskritName={pose.sanskrit_name}
             englishName={pose.english_name}
             diagram={pose.img_url}
+            video={video}
           />)}
         </span>)
        : (
--- src/Components/PoseDetails.js
+++ src/Components/PoseDetails.js
@@ -5,44 +5,29 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Pose } from "./Pose";
 import { useParams } from "react-router-dom";
+import videos from "./utils";
 
 
 export function PoseDetails() {
     const {pose_id} = useParams();
     const [pose, setPose] = useState(null);
     const [poseName, setPoseName] = useState(null);
-    const [video, setVideo] = useState(null);
-    
-    // const KEY=
+    const [video, setVideo] = useState("AB3Y-4a3ZrU");
 
     useEffect(() => {
-        async function getPoseById() {
-            const res = await axios.get(`https://lightning-yoga-api.herokuapp.com/yoga_poses/${pose_id}`)
-            .then((res) => {
-                setPose(res.data);
-                setPoseName(res.data.sanskrit_name)
-            })
-            .catch((error) => {
-                console.error(error)
-            })           
-        }
-        getPoseById()
-    }, [pose_id]);
-
-    console.log(poseName);
-
-    useEffect(() => {
-        async function getVideo() {
-            const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=how+to+do+${poseName}&maxResults=1&key=${KEY}`)
-            .then((res) => {
-                    setVideo(res.data.items[0].id.videoId)             
-            })
-            .catch((error) => {
-                console.error(error)
-            })           
-        }
-        getVideo()
-    }, [poseName]);
+            async function getPoseById() {
+                const res = await axios.get(`https://lightning-yoga-api.herokuapp.com/yoga_poses/${pose_id}`)
+                .then((res) => {
+                    setPose(res.data);
+                    setPoseName(res.data.sanskrit_name);
+                })
+                .catch((error) => {
+                    console.error(error)
+                })           
+            }
+            getPoseById();
+            setVideo(videos[poseName]);
+    }, [pose_id, video, poseName]);
 
     return (
         <>
